dc7ea42e13702e94e4b9ee08a6b2a8541735fcac
Jorge Martinez <jlmartinezpinto@gmail.com>
2024-02-08 19:34:42 -0500

    añadiendo index

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Software de Nómina Web</title>
+    <link rel="stylesheet" href="./cliente/assets/CSS/index.css" />
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="titulo"><h1>Software de Nómina</h1></div>
+        <div class="botton-cont"><a href="./cliente/Usuario/inicioSesion.php" class="button">Inicia Sesión</a></div>
+      </div>
+    </header>
+    <div class="container-section">
+        <section>
+            <h2>Características principales</h2>
+            <ul>
+                <li>Calcula automáticamente salarios y deducciones</li>
+                <li>Genera reportes detallados de nómina</li>
+                <li>Gestiona fácilmente los datos de empleados</li>
+            </ul>
+        </section>
+        <section>
+            <h2>Beneficios</h2>
+            <ul>
+                <li>Ahorra tiempo en el procesamiento de nóminas</li>
+                <li>Minimiza errores en los cálculos</li>
+                <li>
+                Acceso seguro a la información de nómina desde cualquier lugar
+                </li>
+                <li>Soporte técnico y actualizaciones regulares</li>
+            </ul>
+        </section>
+    </div>
+    <footer>
+        <p>&copy; 2024 Software de Nómina. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
